f691105f5ee3e35a951b28f78289f168dc014c6c
Charles Stieffenhofer <c.stieffenhofer@digischool.fr>
2025-01-13 14:00:31 +0100

    chore: Basic setup

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+import { LatestPost } from "~/app/_components/post";
+import { api, HydrateClient } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  void api.post.getLatest.prefetch();
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black text-slate-100">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+          <h1 className="text-5xl font-extrabold tracking-tight">
+            My App
+          </h1>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
