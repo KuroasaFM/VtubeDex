904b6ed560ecbe72804e21dbab81cf0993b009f5
Charles Stieffenhofer <c.stieffenhofer@digischool.fr>
2025-03-20 11:37:16 +0100

    fix: new env var for db envs

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,7 @@ import { Surreal } from "surrealdb";
 const db = new Surreal({
 })
 
-const DATABASE = process.env.NODE_ENV == "production" ? "vtubedex" : "vtubedex-dev";
+const DATABASE = process.env.DB_NAME ?? "vtubedex-dev";
 
 await db.connect("wss://picked-rabbit-069pg8u6bltkndmif8h5q0q0tk.aws-euw1.surreal.cloud", {
   namespace: "kfmed",
